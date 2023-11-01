748ba9b9b00a1e516f9f66e4f3a9ad60caabb822
banchet18 <banjarac6@gmail.com>
2023-11-01 10:52:18 +0530

    color change

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const heading = React.createElement("h1", {}, "hello react World!");
+const heading = React.createElement("h1", { id: "abc" }, "hello react World!");
+
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
