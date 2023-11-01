63a4e9a30a99d6f0af1e3eaae3ea5f33225bc8bc
banchet18 <banjarac6@gmail.com>
2023-11-01 11:07:37 +0530

    nesting tag

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-const heading = React.createElement("h1", { id: "abc" }, "hello react World!");
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+  </div>
+</div>; */
+}
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, `I'm h1 tag`)
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
