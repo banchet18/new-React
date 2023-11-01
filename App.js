0059664f20bab08c5fec19cd9dc0059f7dab1393
banchet18 <banjarac6@gmail.com>
2023-11-01 11:00:55 +0530

    remove console

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 const heading = React.createElement("h1", { id: "abc" }, "hello react World!");
 
-console.log(heading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
