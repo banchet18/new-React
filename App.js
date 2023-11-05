e537a4752157669f986503321cd371ba372570fc
banchet18 <banjarac6@gmail.com>
2023-11-05 16:44:19 +0530

    changes

--- App.js
+++ App.js
@@ -9,12 +9,12 @@
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, `I'm h1 tag`),
-    React.createElement("h2", {}, `I'm h2 tag`),
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, `I'm h1 tag`),
-    React.createElement("h2", {}, `I'm h2 tag`),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
   ]),
 ]);
 
