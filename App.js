e20dacbf9530b12987ed161da120b4febc2c2f3a
banchet18 <banjarac6@gmail.com>
2023-11-06 19:10:06 +0530

    changes

--- App.js
+++ App.js
@@ -1,17 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Starting with React"),
-    React.createElement("h2", {}, "h2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1"),
-    React.createElement("h2", {}, "h2"),
-  ]),
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
