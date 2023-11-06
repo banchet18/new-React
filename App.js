b22ff4e1f48494da4ef6f0977181ef31e1eca1d8
banchet18 <banjarac6@gmail.com>
2023-11-06 19:21:00 +0530

    changes

--- App.js
+++ App.js
@@ -2,3 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React.createElement  =>  Object  =>  HTMLElement(render)
+
+const heading = React.createElement("h1", { id: "heading" }, "helllo channel");
