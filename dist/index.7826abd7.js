336862e48445de73e94d17e3cffea6bee41ffbd8
banchet18 <banjarac6@gmail.com>
2023-11-06 19:21:10 +0530

    c

--- dist/index.7826abd7.js
+++ dist/index.7826abd7.js
@@ -2945,8 +2945,12 @@ try {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
-var _client = require("react-dom/client"); // React.createElement  =>  Object  =>  HTMLElement(render)
+var _client = require("react-dom/client");
 var _clientDefault = parcelHelpers.interopDefault(_client);
+// React.createElement  =>  Object  =>  HTMLElement(render)
+const heading = /*#__PURE__*/ (0, _reactDefault.default).createElement("h1", {
+    id: "heading"
+}, "helllo channel");
 
   $parcel$ReactRefreshHelpers$c1db.postlude(module);
 } finally {
