6b730b50fe9967c4c9af2130be7eab932b52b509
banchet18 <banjarac6@gmail.com>
2023-11-07 16:56:34 +0530

    c

--- App.js
+++ App.js
@@ -19,12 +19,14 @@ import ReactDOM from "react-dom/client";
 const Header = () => {
   return (
     <div className="header">
-      <div className="">
+      <div className="logo-container">
         <img
+          className="logo"
           src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2"
           alt=""
         />
       </div>
+      <div className="nav-items"></div>
     </div>
   );
 };
