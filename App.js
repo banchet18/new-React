d0f819b102b635c2e01d894e28f7b867b0ef8794
banchet18 <banjarac6@gmail.com>
2023-11-07 16:55:30 +0530

    c

--- App.js
+++ App.js
@@ -16,6 +16,27 @@ import ReactDOM from "react-dom/client";
  *  - Contact
  */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="">
+        <img
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <h1></h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
