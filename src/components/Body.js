b91159f93ce01ada115c3195b65a14ec63391926
banchet18 <banjarac6@gmail.com>
2023-12-07 11:04:22 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,17 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "./utils.js/restaurantList";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantList] = useState();
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
+            //filter logic
             restaurantList = restaurantList.filter(
               (res) => res.data.avgRating > 4
             );
