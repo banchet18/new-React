bd02873a93141ce974681cea516f6bc4161a269e
banchet18 <banjarac6@gmail.com>
2023-12-08 11:54:11 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "./utils.js/restaurantList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.002878433409332&lng=73.11106324195862&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // setListOfRestaurant(json.data.card);
+  };
+
+  if (listOfRestaurant.length === 0) {
+    return;
+  }
   return (
     <div className="body">
       <div className="filter">
