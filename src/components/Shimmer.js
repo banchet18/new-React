01641ff8f8acbf959fa7132acc2629f76397434d
banchet18 <banjarac6@gmail.com>
2023-12-08 11:55:56 +0530

    changes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,1 +1,17 @@
-const Shimmer = () => {};
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
