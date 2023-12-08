1b0f61c79573fa105f452e658ab73b471cf99b51
banchet18 <banjarac6@gmail.com>
2023-12-08 09:20:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,32 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-const RestaurantCard = (props) => {
-  let { resdata } = props;
-  return (
-    <div
-      className="res-card"
-      style={{
-        backgroundColor: "#f0f0f0",
-      }}
-    >
-      <img
-        className="res-logo"
-        src={
-          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-          resdata.data.cloudinaryImageId
-        }
-        alt="res-logo"
-      />
-      <h3>{resdata.data.name}</h3>
-      <h4>{resdata.data.cuisines.join(", ")}</h4>
-      <h4>{resdata.data.avgRating}</h4>
-      <h4>{resdata.data.costForTwo / 100}</h4>
-      <h4>{resdata.data.deliveryTime}</h4>
-    </div>
-  );
-};
-
 const AppLayout = () => {
   return (
     <div className="app">
